ac02489a24569f9488259c71e69dd89408b014da
jose andres <contretaa@gmail.com>
2022-03-25 13:01:30 -0400

    refactor: check user token
    Signed-off-by: jose andres <contretaa@gmail.com>

--- src/helpers/decodedTokenAuth.ts
+++ src/helpers/decodedTokenAuth.ts
@@ -1,4 +1,4 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
-export const decodedToken = (token: string): string | JwtPayload =>
+export const tokenDecode = (token: string): string | JwtPayload =>
   jwt.verify(token, process.env.SECRET_KEY as string);
--- src/middlewares/authorizer.ts
+++ src/middlewares/authorizer.ts
@@ -1,26 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpStatusCode } from '../enums/HttpStatusCode';
 import { BaseError } from '../Exceptions/BaseError';
-import { decodedToken } from '../helpers/decodedTokenAuth';
+import { tokenDecode } from '../helpers/decodedTokenAuth';
 
 export const authorizer = (req: Request, res: Response, next: NextFunction) => {
-  let token = null;
   const authorization = req.headers['authorization'];
-  console.log(authorization);
-  try {
-    if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-      token = authorization.split(' ')[1];
-    }
 
-    if (!token)
-      throw new BaseError(HttpStatusCode.UNAUTHORIZED, 'token invalid');
+  try {
+    const hasAuthHeader =
+      authorization && authorization.toLowerCase().startsWith('bearer');
+
+    if (!hasAuthHeader)
+      throw new BaseError(HttpStatusCode.UNAUTHORIZED, 'missing token');
+
+    const [_, token] = authorization.split(' ');
 
     console.log(token);
+    if (!token) {
+      throw new BaseError(HttpStatusCode.UNAUTHORIZED, 'missing token');
+    }
 
-    const tokenDecoded = decodedToken(token);
+    const decodedToken = tokenDecode(token);
 
-    if (!tokenDecoded)
-      throw new BaseError(HttpStatusCode.UNAUTHORIZED, 'token missing');
+    if (!decodedToken)
+      throw new BaseError(HttpStatusCode.UNAUTHORIZED, 'invalid token');
+
+    console.log(decodedToken);
 
     next();
   } catch (error) {
